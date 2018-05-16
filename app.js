e64148a16f009866c7ab6b18c66d862cc374b5ea
Simon Seo <sseo7248@gmail.com>
2018-05-16 13:01:25 -0700

    project files added

---
+++ app.js
@@ -0,0 +1,5 @@
+const myHeading = document.getElementById('myHeading');
+const myButton = document.getElementById('myButton');
+
+myButton.addEventListener('click', () => { myHeading.style.color = 'red';});
+ 
