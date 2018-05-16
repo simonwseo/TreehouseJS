fed14148ae2276b8c910ead62ca73098f81ead08
Simon Seo <sseo7248@gmail.com>
2018-05-16 14:37:10 -0700

    This commit will: add new examples for selecting elements

--- app2.js
+++ app2.js
@@ -7,3 +7,10 @@ const myList = document.getElementsByTagName('li');
 for (let i = 0; i < myList.length; i++){
   myList[i].style.color = 'purple';
 }
+
+//Selecting elements through their class Names
+const errorNotPurple = document.getElementsByClassName('error-not-purple');
+
+for (let i = 0; i < myList.length; i++){
+  errorNotPurple[i].style.color = 'red';
+}
