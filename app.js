91ac25cb465aa396592cd169bd0fe3ed963285ac
Simon Seo <sseo7248@gmail.com>
2018-05-16 13:04:05 -0700

    minor change

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ const myHeading = document.getElementById('myHeading');
 const myButton = document.getElementById('myButton');
 
 myButton.addEventListener('click', () => { myHeading.style.color = 'red';});
- 
